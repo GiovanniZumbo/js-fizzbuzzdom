71cd8629f042e969b43dab0221159fd59f6730f4
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-02 15:52:07 +0200

    for cycle

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // PLANNING
 
 /* 
-Creo una variabile per l'Ul
-Creo un ciclo for per generare gli <li> con dentro i numeri
-Aggiungo le condizioni per i multipli
-Stampo in console 
+Creo una variabile per l'Ul / Create a <ul> var
+Creo un ciclo for per generare gli <li> con dentro i numeri / Create a for cycle and generate <li> elements
+Aggiungo le condizioni per i multipli / Ad id for multiplies    
+Stampo in console / Console output
  */
 
 
@@ -12,10 +12,18 @@ Stampo in console
 // # GATHERING DATA
 // # EVENT HANDLING
 // # PROCESSING
+
+// * Creo una variabile per l'Ul / Create a <ul> var
+
 let numberList = '<ul>'
 console.log(numberList);
 
+// * Creo un ciclo for per generare gli <li> con dentro i numeri / Create a for cycle and generate <li> elements
 
+for (i = 1; i <= 100; i++) {
+    numberList += `<li> ${i} </li>`;
+    console.log(numberList)
+}
 
 
 // # OUTPUT     
