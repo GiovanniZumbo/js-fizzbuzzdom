93f7fbaeb45ce39dda54a1aae7e71866bfac84dc
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-02 15:47:31 +0200

    Add let <ul>

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // PLANNING
 
-/* */
+/* 
+Creo una variabile per l'Ul
+Creo un ciclo for per generare gli <li> con dentro i numeri
+Aggiungo le condizioni per i multipli
+Stampo in console 
+ */
 
 
 // # STAGING
 // # GATHERING DATA
+// # EVENT HANDLING
 // # PROCESSING
-// # OUTPUT 
+let numberList = '<ul>'
+console.log(numberList);
+
+
+
+
+// # OUTPUT     
